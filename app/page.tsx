d3fb3b03f2eaa2739cb81de01678febb9c6597dd
mamun060 <almamun.raju520@gmail.com>
2023-11-28 15:31:53 +0600

    check update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 
-export function Page() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
     <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
